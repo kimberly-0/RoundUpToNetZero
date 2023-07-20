2c983abf3d7c4344a947c29eb96f707b5ad1f7a0
Kimberly Dijkmans <kim.14420@outlook.com>
2023-07-20 14:30:49 +0200

    move side nav into main

--- client/src/views/layout/Layout.js
+++ client/src/views/layout/Layout.js
@@ -3,13 +3,14 @@ import SideNav from './SideNav';
 
 const Layout = ({ children }) => {
     return(
-        <>
+        <div className='layout'>
             <Header />
 
-            <SideNav />
-
-            <main>{ children }</main>
-        </>
+            <main className='main'>
+                <SideNav />
+                <div className='page-content'>{ children }</div>
+            </main>
+        </div>
     )
 }
 
