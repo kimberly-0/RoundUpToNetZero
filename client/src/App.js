809f3c2df419f7288d77ba0ee62b31556408ea83
Kimberly Dijkmans <kim.14420@outlook.com>
2023-07-20 13:06:33 +0200

    add pages to routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
+import LogIn from './views/pages/LogIn';
+import Dashboard from './views/pages/Dashboard';
+import TransactionHistory from './views/pages/TransactionHistory';
+import InvestmentHistory from './views/pages/InvestmentHistory';
+import Invest from './views/pages/Invest';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<h1>Start screen</h1>} />
-        <Route path='/dashboard' element={<h1>Dashboard</h1>} />
+        <Route path='/' element={<LogIn />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/transaction-history' element={<TransactionHistory />} />
+        <Route path='/investment-history' element={<InvestmentHistory />} />
+        <Route path='/invest' element={<Invest />} />
       </Routes>
     </div>
   );
