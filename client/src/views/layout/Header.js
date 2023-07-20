0f3277b2d820b844f3bab780c231aec6787197aa
Kimberly Dijkmans <kim.14420@outlook.com>
2023-07-20 23:38:29 +0200

    change a to button for notifications

--- client/src/views/layout/Header.js
+++ client/src/views/layout/Header.js
@@ -8,7 +8,7 @@ function Header() {
                 <p className='logo'>0</p>
                 <h1 className='title'>Round Up to <span className='highlighted'>Net</span> Zero</h1>
             </div>
-            <a href='#' className='notifications-button'><FaBell /></a>
+            <button className='notifications-button'><FaBell /></button>
         </header>
     )
 }
