16b8753e8a205213394466755da34c0b4f0c9682
Kimberly Dijkmans <kim.14420@outlook.com>
2023-07-20 14:32:57 +0200

    add NavLinks

--- client/src/views/layout/SideNav.js
+++ client/src/views/layout/SideNav.js
@@ -1,7 +1,62 @@
+import { NavLink } from 'react-router-dom'
+
 function SideNav() {
     return (
         <nav className='side-nav'>
+            <div className='side-nav__top'>
+                <NavLink 
+                    to='/dashboard'
+                    className={({ isActive, isPending }) =>
+                        isPending ? 'pending' : isActive ? 'active' : ''
+                    }
+                >
+                    Dashboard
+                </NavLink>
 
+                <NavLink 
+                    to='/transaction-history'
+                    className={({ isActive, isPending }) =>
+                        isPending ? 'pending' : isActive ? 'active' : ''
+                    }
+                >
+                    Transaction history
+                </NavLink>
+                <NavLink 
+                    to='/investment-history'
+                    className={({ isActive, isPending }) =>
+                        isPending ? 'pending' : isActive ? 'active' : ''
+                    }
+                >
+                    Investment history
+                </NavLink>
+                <NavLink 
+                    to='/invest'
+                    className={({ isActive, isPending }) =>
+                        isPending ? 'pending' : isActive ? 'active' : ''
+                    }
+                >
+                    Invest now
+                </NavLink>
+            </div>
+
+            <div className='side-nav__bottom'>
+                <NavLink 
+                    to='/settings'
+                    className={({ isActive, isPending }) =>
+                        isPending ? 'pending' : isActive ? 'active' : ''
+                    }
+                >
+                    Settings
+                </NavLink>
+                <NavLink 
+                    to='/log-in'
+                    className={({ isActive, isPending }) =>
+                        isPending ? 'pending' : isActive ? 'active' : ''
+                    }
+                >
+                    Log out
+                </NavLink>
+            </div>
         </nav>            
     )
 }
