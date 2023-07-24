c39ceed4e26f9082c1cfdd267e3c32aeef240768
Kimberly Dijkmans <kim.14420@outlook.com>
2023-07-24 16:51:09 +0200

    add user router and methods

--- server/api/api.js
+++ server/api/api.js
@@ -1,5 +1,12 @@
-const express = require('express')
-const app = require('../server')
+const express = require('express');
+const app = require('../server');
 
-const apiRouter = express.Router()
-module.exports = apiRouter
+const apiRouter = express.Router();
+module.exports = apiRouter;
+
+const onRequestHook = require('./activeUser');
+apiRouter.use(onRequestHook); // fake user login before each request
+
+const usersRouter = require('./users');
+apiRouter.use('/users', usersRouter);
+
