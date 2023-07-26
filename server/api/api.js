df425bf3991127fb2b2673c6c0fc1f1748307237
Kimberly Dijkmans <kim.14420@outlook.com>
2023-07-26 14:34:12 +0200

    add remaining routers

--- server/api/api.js
+++ server/api/api.js
@@ -7,6 +7,23 @@ module.exports = apiRouter;
 const onRequestHook = require('./activeUser');
 apiRouter.use(onRequestHook); // fake user login before each request
 
+const investmentsRouter = require('./investments');
+apiRouter.use('/investments', investmentsRouter);
+
+const companiesRouter = require('./companies');
+apiRouter.use('/companies', companiesRouter);
+
 const usersRouter = require('./users');
 apiRouter.use('/users', usersRouter);
 
+const paymentsRouter = require('./payments');
+usersRouter.use('/:userId/payments', paymentsRouter);
+
+const transactionsRouter = require('./transactions');
+usersRouter.use('/:userId/transactions', transactionsRouter);
+
+const userInvestmentsRouter = require('./user_investments');
+usersRouter.use('/:userId/investments', userInvestmentsRouter);
+
+const userCompaniesRouter = require('./user_companies');
+usersRouter.use('/:userId/companies', userCompaniesRouter);
