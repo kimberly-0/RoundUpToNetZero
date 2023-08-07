68868794005ba81d2235c9290ef804a51d7002a1
Kimberly Dijkmans <kim.14420@outlook.com>
2023-08-07 11:21:57 +0100

    add method to retrieve single transaction

--- client/src/services/userTransactions.js
+++ client/src/services/userTransactions.js
@@ -1,5 +1,12 @@
 import { makeRequest  } from './makeRequest';
 
+export function getTransactionById({ userId, transactionId }) {
+    return makeRequest(`/users/${userId}/transactions/${transactionId}`).catch(error => {
+        console.log("Unable to retrieve transaction");
+        return [];
+    });
+}
+
 export function getTransactions({ userId }) {
     return makeRequest(`/users/${userId}/transactions`).catch(error => {
         console.log("Unable to retrieve transactions");
