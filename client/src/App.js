fdda97467e447b92a42a9d001fd45c759d63e293
Kimberly Dijkmans <kim.14420@outlook.com>
2023-08-12 11:28:58 +0100

    change trans. path + add single investment route

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import LogIn from './views/pages/LogIn';
 import Dashboard from './views/pages/Dashboard';
 import TransactionHistory from './views/pages/TransactionHistory';
 import SingleTransaction from './views/pages/SingleTransaction';
+import SingleInvestment from './views/pages/SingleInvestment';
 import AddTransaction from './views/pages/AddTransaction';
 import EditTransaction from './views/pages/EditTransaction';
 import InvestmentHistory from './views/pages/InvestmentHistory';
@@ -21,12 +22,13 @@ function App() {
         <Route path='/' element={ <Navigate to={loggedInUserId ? "/dashboard" : "/log-in"} />  } />
         <Route path='/log-in' element={ <LogIn /> } />
         <Route path='/dashboard' element={ <Dashboard userId={loggedInUserId}/> } />
-        <Route path='/transaction-history' element={ <TransactionHistory userId={loggedInUserId}/> } />
-        <Route path='/transaction/:id' element={ <SingleTransaction userId={loggedInUserId}/> } />
+        <Route path='/transactions' element={ <TransactionHistory userId={loggedInUserId}/> } />
+        <Route path='/transactions/:id' element={ <SingleTransaction userId={loggedInUserId}/> } />
         <Route path='/edit-transaction/:id' element={ <EditTransaction userId={loggedInUserId}/> } />
         <Route path='/add-transaction' element={ <AddTransaction userId={loggedInUserId}/> } />
         <Route path='/investment-history' element={ <InvestmentHistory userId={loggedInUserId}/> } />
         <Route path='/invest' element={ <Invest userId={loggedInUserId}/> } />
+        <Route path='/invest/:id' element={ <SingleInvestment userId={loggedInUserId}/> } />
         <Route path='/settings' element={ <Settings userId={loggedInUserId}/> } />
       </Routes>
     </div>
