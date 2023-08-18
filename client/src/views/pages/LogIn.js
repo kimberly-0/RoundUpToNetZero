21297ecd234d36542ae4a6eee2e8fdaae6fef9ae
Kimberly Dijkmans <kim.14420@outlook.com>
2023-08-18 09:52:47 +0100

    create visual login form

--- client/src/views/pages/LogIn.js
+++ client/src/views/pages/LogIn.js
@@ -1,11 +1,62 @@
-import Layout from '../layout/Layout';
+import { useState } from 'react';
+import Header from '../layout/Header';
 
 function LogIn() {
-  return (
-    <Layout>
-        <h2 className='page-title'>Log In</h2>
-    </Layout>
-  );
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <div className='layout'>
+
+            <Header showNotificationButton={false} />
+
+            <main className='main'>
+                <div className='page-content small'>
+
+                    <h2 className='page-title'>Log In</h2>
+
+                    <div className='page-table-container component-container'>
+                        <form className='login-form'>
+                            <div className='input-container'>
+                                <label htmlFor="username">Username</label>
+                                <input
+                                    type='username'
+                                    id='username'
+                                    name='username'
+                                    value={username}
+                                    onChange={e => setUsername(e.target.value)}
+                                    placeholder='Username'
+                                />
+                            </div>
+
+                            <div className='input-container'>
+                                <label htmlFor="password">Password</label>
+                                <input
+                                    type='password'
+                                    id='password'
+                                    name='password'
+                                    value={password}
+                                    onChange={e => setPassword(e.target.value)}
+                                    placeholder='Password'
+                                />
+                            </div>
+
+                            <div className='button-section'>
+                                {/* <p className={`error-msg ${!error ? "hide" : ""}`}>{error}</p> */}
+
+                                <button 
+                                    className='form-button rounded-button coloured' 
+                                    type='submit'
+                                >Log in</button>
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default LogIn;
