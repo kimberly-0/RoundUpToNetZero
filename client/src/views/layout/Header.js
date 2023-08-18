555d817f8e6e30bc693b7a9247f5f35e0cece221
Kimberly Dijkmans <kim.14420@outlook.com>
2023-08-18 09:52:57 +0100

    add condition for optional notifications button

--- client/src/views/layout/Header.js
+++ client/src/views/layout/Header.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { FaBell } from 'react-icons/fa';
 
-function Header() {
+function Header({ showNotificationButton = true }) {
     return (
         <header className='header'>
             <div className='header__title-box'>
                 <p className='logo'>0</p>
                 <h1 className='title'>Round Up to <span className='highlighted'>Net</span> Zero</h1>
             </div>
-            <button className='notifications-button'><FaBell /></button>
+            {showNotificationButton && <button className='notifications-button'><FaBell /></button>}
         </header>
     )
 }
