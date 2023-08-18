0670b8ebcf8b6eb8f52f8ee1c1079fae3858183b
Kimberly Dijkmans <kim.14420@outlook.com>
2023-08-18 16:42:02 +0100

    remove JSON parsing

--- client/src/hooks/useToken.js
+++ client/src/hooks/useToken.js
@@ -4,15 +4,14 @@ export function useToken() {
 
     const getToken = () => {
         const tokenString = localStorage.getItem('token');
-        const userToken = JSON.parse(tokenString);
-        return userToken?.token;
+        return tokenString;
     };
 
     const [token, setToken] = useState(getToken());
 
     const saveToken = userToken => {
         localStorage.setItem('token', JSON.stringify(userToken));
-        setToken(userToken.token);
+        setToken(userToken);
     };
 
     return {
