543aa9dd50413f0892c7445ff372c564a153b12a
Kimberly Dijkmans <kim.14420@outlook.com>
2023-08-18 16:43:46 +0100

    comment unnecessary code temp

--- server/api/activeUser.js
+++ server/api/activeUser.js
@@ -1,29 +1,29 @@
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
+// const { PrismaClient } = require('@prisma/client')
+// const prisma = new PrismaClient()
 
-const loggedInUserName = 'John Doe'; // Change name to log in with a different user
+// const loggedInUserName = 'John Doe'; // Change name to log in with a different user
 
-// fake user login before each request
-async function onRequestHook (req, res, next) {
-    const CURRENT_USER_ID = (
-        await prisma.user.findFirst({ where: { name: loggedInUserName } }).then(user => {
-            return user.id
-        }).catch(error => {
-            console.log(error)
-            return
-        })
-    ) 
+// // fake user login before each request
+// async function onRequestHook (req, res, next) {
+//     const CURRENT_USER_ID = (
+//         await prisma.user.findFirst({ where: { name: loggedInUserName } }).then(user => {
+//             return user.id
+//         }).catch(error => {
+//             console.log(error)
+//             return
+//         })
+//     ) 
 
-    if (req.cookies.userId !== CURRENT_USER_ID) {
-        req.cookies.userId = CURRENT_USER_ID
-        res.clearCookie("userId")
-        res.cookie("userId", CURRENT_USER_ID)
-    }
+//     if (req.cookies.userId !== CURRENT_USER_ID) {
+//         req.cookies.userId = CURRENT_USER_ID
+//         res.clearCookie("userId")
+//         res.cookie("userId", CURRENT_USER_ID)
+//     }
     
-    // console.log("req.cookies.userId : " + req.cookies.userId)
-    // console.log("req.signedCookies : " + req.signedCookies.userId)
+//     // console.log("req.cookies.userId : " + req.cookies.userId)
+//     // console.log("req.signedCookies : " + req.signedCookies.userId)
 
-    next()
-}
+//     next()
+// }
 
-module.exports = onRequestHook;
+// module.exports = onRequestHook;
--- server/api/api.js
+++ server/api/api.js
@@ -4,8 +4,8 @@ const app = require('../server');
 const apiRouter = express.Router();
 module.exports = apiRouter;
 
-const onRequestHook = require('./activeUser');
-apiRouter.use(onRequestHook); // fake user login before each request
+// const onRequestHook = require('./activeUser');
+// apiRouter.use(onRequestHook); // fake user login before each request
 
 const loginRouter = require('./login');
 apiRouter.use('/login', loginRouter);
