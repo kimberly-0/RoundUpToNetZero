f434eef151ecc9c3fedec391a4e559baf183db49
Kimberly Dijkmans <kim.14420@outlook.com>
2023-08-19 11:31:05 +0100

    remove debugging code

--- client/src/hooks/useUser.js
+++ client/src/hooks/useUser.js
@@ -1,28 +1,9 @@
 import { useState } from 'react';
-import { makeRequest } from "../services/makeRequest";
 
 export function useUser() {
 
-    // const [userId, setUserId] = useState();
-
     const getUserId = () => {
         const userIdFromLocalStorage = localStorage.getItem('userId');
-
-        // Get userId using a request if there is no userId in local storage (local storage is not supported for cross-domain deployment)
-        if (!userIdFromLocalStorage) {
-            return makeRequest(`${process.env.REACT_APP_SERVER_URL}/login`, {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ email: 'john.doe@email.com', password: 'fakepassword' }),
-            })
-            .then(response => {
-                if (response.status !== 200) throw new Error("Invalid credentials");
-                return response.json();
-            });
-        }
-
         return userIdFromLocalStorage;
     };
 
