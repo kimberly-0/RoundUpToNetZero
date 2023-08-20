d1a8d55684c8ae91312b852d1113593ea37b7c87
Kimberly Dijkmans <kim.14420@outlook.com>
2023-08-20 17:31:53 +0100

    add method to create a company and assign to user

--- client/src/services/company.js
+++ client/src/services/company.js
@@ -1,4 +1,14 @@
 import { makeRequest } from './makeRequest'
+import { updateUser } from './user';
+
+export function createCompany({ userId, company }) {
+    return makeRequest(`/companies`, {
+        method: "POST",
+        data: { company: company },
+    }).then(company => {
+        return updateUser({ userId, user: { companyId: company.id } });
+    });
+}
 
 export function updateCompany({ companyId, company }) {
     return makeRequest(`/companies/${companyId}`, {
