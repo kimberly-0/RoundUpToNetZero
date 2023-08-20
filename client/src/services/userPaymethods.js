b97d77337a409e81dc329d67308a04ca7660e11c
Kimberly Dijkmans <kim.14420@outlook.com>
2023-08-20 15:34:06 +0100

    add methods to get paymethod by ID and delete

--- client/src/services/userPaymethods.js
+++ client/src/services/userPaymethods.js
@@ -4,6 +4,13 @@ export function getPaymethods({ userId }) {
     return makeRequest(`/users/${userId}/paymethods`)
 }
 
+export function getPaymethodById({ userId, paymethodId }) {
+    return makeRequest(`/users/${userId}/paymethods/${paymethodId}`).catch(error => {
+        console.log("Unable to retrieve payment method");
+        return null;
+    });
+}
+
 export function createPaymethod({ userId, paymethod }) {
     return makeRequest(`/users/${userId}/paymethods`, {
         method: "POST",
@@ -15,5 +22,11 @@ export function updatePaymethod({ userId, paymethodId, paymethod }) {
     return makeRequest(`/users/${userId}/paymethods/${paymethodId}`, {
         method: "PUT",
         data: { paymethod: paymethod },
+    });
+}
+
+export function deletePaymethod({ userId, paymethodId }) {    
+    return makeRequest(`/users/${userId}/paymethods/${paymethodId}`, {
+        method: "DELETE",
     });
 }
